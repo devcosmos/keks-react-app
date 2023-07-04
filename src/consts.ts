c04d3e9c586b2c90f8042a644365b38b0a1d63c8
cosmos <va-litv@yandex.ru>
2023-07-04 17:00:48 +0300

    add app routes

---
+++ src/consts.ts
@@ -0,0 +1,10 @@
+export enum AppRoute {
+  Main = '/',
+  Catalog = '/catalog',
+  Product = '/catalog/:id',
+  Favourites = '/favourites',
+  NotFound = '/404',
+  Login = '/login',
+  Signup = '/signup',
+  Error = '/error'
+}
