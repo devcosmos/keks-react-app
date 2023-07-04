613a02db8573ed4e1826e2092c66eab261b39dc1
cosmos <va-litv@yandex.ru>
2023-07-04 17:01:16 +0300

    add base browser router

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppRoute } from '../../consts';
+import Main from '../../pages/main/main';
+
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main} element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
