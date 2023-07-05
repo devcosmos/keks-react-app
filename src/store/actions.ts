c3f1569ae446e8d7bb44debc519bc8c89f896656
cosmos <va-litv@yandex.ru>
2023-07-05 12:34:46 +0300

    update reducer and actions

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,3 +1,5 @@
 import { createAction } from '@reduxjs/toolkit';
+import { Products } from '../types/products';
 
 export const changeFilterBase = createAction<string>('catalog/changeFilterBase');
+export const loadProducts = createAction<Products>('data/loadProducts');
