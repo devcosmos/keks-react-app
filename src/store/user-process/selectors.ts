5e995b76a1fb6d9152be2483ab6e03c62185fc2d
cosmos <va-litv@yandex.ru>
2023-07-05 18:08:52 +0300

    change import

--- src/store/user-process/selectors.ts
+++ src/store/user-process/selectors.ts
@@ -1,5 +1,5 @@
 import { AuthStatus, NameSpace } from '../../consts';
-import { UserData } from '../../types/data';
+import { UserData } from '../../types/users';
 import { State } from '../../types/state';
 
 export const getAuthStatus = (state: State): AuthStatus => state[NameSpace.User].authorizationStatus;
