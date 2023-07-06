d71e1997175ef8030a3893d5e56acafb7289c7b4
cosmos <va-litv@yandex.ru>
2023-07-06 14:32:54 +0300

    add private route

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-import { AppRoute } from '../../consts';
+import { AppRoute, AuthStatus } from '../../consts';
 import Main from '../../pages/main/main';
 import Catalog from '../../pages/catalog/catalog';
 import Favourites from '../../pages/favourites/favourites';
@@ -7,16 +7,48 @@ import NotFound from '../../pages/not-found/not-found';
 import Product from '../../pages/product/product';
 import SignIn from '../../pages/sign-in/sign-in';
 import SignUp from '../../pages/sign-up/sign-up';
+import { getAuthStatus } from '../../store/user-process/selectors';
+import { useAppSelector } from '../../hooks';
+import PrivateRoute from '../private-route/private-route';
+import Loader from '../loader/loader';
 
 function App(): JSX.Element {
+  const authStatus = useAppSelector(getAuthStatus);
+  const isAuth = authStatus === AuthStatus.Auth;
+
+  if (authStatus === AuthStatus.Unknown) {
+    return <Loader />;
+  }
+
   return (
     <Routes>
       <Route path={AppRoute.Main} element={<Main />} />
       <Route path={AppRoute.Catalog} element={<Catalog />} />
       <Route path={AppRoute.Product} element={<Product />} />
-      <Route path={AppRoute.Favourites} element={<Favourites />} />
-      <Route path={AppRoute.SignIn} element={<SignIn />} />
-      <Route path={AppRoute.SignUp} element={<SignUp />} />
+      <Route
+        path={AppRoute.Favourites}
+        element={
+          <PrivateRoute access={isAuth} redirect={AppRoute.SignIn}>
+            <Favourites />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path={AppRoute.SignIn}
+        element={
+          <PrivateRoute access={!isAuth} redirect={AppRoute.Main}>
+            <SignIn />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path={AppRoute.SignUp}
+        element={
+          <PrivateRoute access={!isAuth} redirect={AppRoute.Main}>
+            <SignUp />
+          </PrivateRoute>
+        }
+      />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
