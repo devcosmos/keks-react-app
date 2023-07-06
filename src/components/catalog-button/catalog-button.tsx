57439a7e6c22caeb7a9e5795997f44048623ab92
cosmos <va-litv@yandex.ru>
2023-07-06 22:08:35 +0300

    add catalog button

---
+++ src/components/catalog-button/catalog-button.tsx
@@ -0,0 +1,33 @@
+import { CATALOG_PRODUCT_DISPLAY_COUNT } from '../../consts';
+
+type MoreButtonProps = {
+  productsCount: number;
+  showCount: number;
+  setShowCount: (count: number) => void;
+}
+
+function CatalogButton({productsCount, showCount, setShowCount}: MoreButtonProps): JSX.Element {
+  return (
+    <div className="catalog__button-wrapper">
+      {productsCount > showCount ? (
+        <button
+          className="btn btn--second"
+          type="button"
+          onClick={() => setShowCount(showCount + CATALOG_PRODUCT_DISPLAY_COUNT)}
+        >
+            Показать еще
+        </button>
+      ) : (
+        <button
+          className="btn btn--second"
+          type="button"
+          onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+        >
+          В начало
+        </button>
+      )}
+    </div>
+  );
+}
+
+export default CatalogButton;
--- src/components/catalog-list/catalog-list.tsx
+++ src/components/catalog-list/catalog-list.tsx
@@ -1,26 +1,36 @@
+import { useState } from 'react';
 import { Products } from '../../types/products';
+import { CATALOG_PRODUCT_DISPLAY_COUNT } from '../../consts';
+import CatalogButton from '../catalog-button/catalog-button';
 import CardItem from '../card-item/card-item';
 
 type CatalogListProps = {
   products: Products;
+  showAll?: boolean;
 }
 
-function CatalogList({products}: CatalogListProps): JSX.Element {
+function CatalogList({products, showAll = false}: CatalogListProps): JSX.Element {
+  const initialCount = showAll ? products.length : CATALOG_PRODUCT_DISPLAY_COUNT;
+  const [showCount, setShowCount] = useState<number>(initialCount);
+
   return (
     <section className="catalog">
       <div className="container">
         <h2 className="visually-hidden">Каталог</h2>
         <div className="catalog__wrapper">
           <ul className="catalog__list">
-            {products.map((product) => (
+            {products.slice(0, showCount).map((product) => (
               <li key={product.id} className="catalog__item">
-                <CardItem product={product} isBigCard/>
+                <CardItem product={product} isBigCard />
               </li>
             ))}
           </ul>
-          <div className="catalog__button-wrapper">
-            <button className="btn btn--second" type="button">Показать еще</button>
-          </div>
+          {!showAll &&
+            <CatalogButton
+              productsCount={products.length}
+              showCount={showCount}
+              setShowCount={setShowCount}
+            />}
         </div>
       </div>
     </section>
