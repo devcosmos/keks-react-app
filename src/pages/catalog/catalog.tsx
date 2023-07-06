19b76101dd3109efa54ed3064152b924bb1d965b
cosmos <va-litv@yandex.ru>
2023-07-07 00:01:02 +0300

    add fetch categories to catalog

--- src/pages/catalog/catalog.tsx
+++ src/pages/catalog/catalog.tsx
@@ -1,13 +1,26 @@
+import { useEffect } from 'react';
 import CatalogFilter from '../../components/catalog-filter/catalog-filter';
 import CatalogList from '../../components/catalog-list/catalog-list';
 import Layout from '../../components/layout/layout';
 import { useAppSelector } from '../../hooks';
-import { getErrorStatus, getProducts } from '../../store/products-data/selectors';
+import { getErrorStatus, getLoadingStatus, getProducts } from '../../store/products-data/selectors';
 import Error from '../error/error';
+import { store } from '../../store';
+import { fetchCategoriesAction } from '../../store/api-actions';
+import Loader from '../../components/loader/loader';
 
 function Catalog(): JSX.Element {
   const products = useAppSelector(getProducts);
   const isError = useAppSelector(getErrorStatus);
+  const isLoading = useAppSelector(getLoadingStatus);
+
+  useEffect(() => {
+    store.dispatch(fetchCategoriesAction());
+  }, []);
+
+  if (isLoading) {
+    return <Loader />;
+  }
 
   if (isError) {
     return <Error />;
