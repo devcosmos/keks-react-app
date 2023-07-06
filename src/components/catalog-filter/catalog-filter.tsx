383f1a6e456b850a928f4e29f8a52c8f1d76ce02
cosmos <va-litv@yandex.ru>
2023-07-07 00:01:36 +0300

    update categories in filter 2:30

--- src/components/catalog-filter/catalog-filter.tsx
+++ src/components/catalog-filter/catalog-filter.tsx
@@ -1,12 +1,14 @@
 import classNames from 'classnames';
-import { BASICS } from '../../consts';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { changeFilterBase } from '../../store/products-process/products-process';
 import { getFilterBase } from '../../store/products-process/selectors';
+import { getCategories } from '../../store/products-data/selectors';
+import { ProductCategoryInRUS } from '../../consts';
 
 function CatalogFilter(): JSX.Element {
   const dispatch = useAppDispatch();
-  const activeBase = useAppSelector(getFilterBase);
+  const activeCategory = useAppSelector(getFilterBase);
+  const categories = useAppSelector(getCategories);
 
   return (
     <div className="catalog-filter">
@@ -14,20 +16,20 @@ function CatalogFilter(): JSX.Element {
         <div className="catalog-filter__first-level">
           <h3 className="catalog-filter__title catalog-filter__title--first-level">основы</h3>
           <ul className="catalog-filter__list catalog-filter__list--first-level">
-            {BASICS.map((base) => (
-              <li key={base} className="catalog-filter__item catalog-filter__item--first-level">
+            {categories.map((item) => (
+              <li key={item.category} className="catalog-filter__item catalog-filter__item--first-level">
                 <button
                   type="button"
                   className={classNames(
                     'btn btn--filter-first-level',
-                    {'is-active': activeBase === base}
+                    {'is-active': activeCategory === item.category}
                   )}
                   onClick={(evt) => {
                     evt.preventDefault();
-                    dispatch(changeFilterBase(base));
+                    dispatch(changeFilterBase(item.category));
                   }}
                 >
-                  {base}
+                  {ProductCategoryInRUS[item.category]}
                 </button>
               </li>
             ))}
