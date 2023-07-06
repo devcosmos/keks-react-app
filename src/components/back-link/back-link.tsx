1a4308dd160e1084ba191d3fac60eb851fa6641a
cosmos <va-litv@yandex.ru>
2023-07-06 22:08:45 +0300

    update back link

--- src/components/back-link/back-link.tsx
+++ src/components/back-link/back-link.tsx
@@ -1,11 +1,13 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { AppRoute } from '../../consts';
 
 function BackLink(): JSX.Element {
+  const navigate = useNavigate();
+
   return (
     <div className="back-link">
       <div className="container">
-        <Link className="back-link__link" to={AppRoute.Main}>
+        <Link className="back-link__link" to={AppRoute.Main} onClick={() => navigate(-1)}>
           Назад
           <svg className="back-link__icon" width="30" height="16" aria-hidden="true">
             <use xlinkHref="#icon-arrow-left" />
