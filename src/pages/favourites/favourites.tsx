e740582d020916ace17b871d765d7c5393d0e38e
cosmos <va-litv@yandex.ru>
2023-07-07 20:16:25 +0300

    update favourite page

--- src/pages/favourites/favourites.tsx
+++ src/pages/favourites/favourites.tsx
@@ -1,22 +1,45 @@
-// import CatalogList from '../../components/catalog-list/catalog-list';
+import CatalogList from '../../components/catalog-list/catalog-list';
+import FavouritesEmpty from '../../components/favourites-empty/favourites-empty';
 import FavouritesQuantity from '../../components/favourites-quantity/favourites-quantity';
 import Layout from '../../components/layout/layout';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { deleteFavoriteAction } from '../../store/api-actions';
+import { getFavorites } from '../../store/favorites-data/selectors';
 
 function Favourites(): JSX.Element {
+  const dispatch = useAppDispatch();
+
+  const favorites = useAppSelector(getFavorites);
+
+  const handleClearButton = () =>
+    favorites.forEach((product) => {
+      dispatch(deleteFavoriteAction(product.id));
+    });
+
   return (
     <Layout header heading="Избранное" backLink footer>
-      <>
-        <FavouritesQuantity />
-        <section className="favourites">
-          <div className="container">
-            <h2 className="visually-hidden">Избранные товары</h2>
-            <div className="favourites__button">
-              <button className="btn btn--second" type="button">Очистить</button>
+      {favorites.length ? (
+        <>
+          <FavouritesQuantity favorites={favorites} />
+          <section className="favourites">
+            <div className="container">
+              <h2 className="visually-hidden">Избранные товары</h2>
+              <div className="favourites__button">
+                <button
+                  className="btn btn--second"
+                  type="button"
+                  onClick={handleClearButton}
+                >
+                  Очистить
+                </button>
+              </div>
             </div>
-          </div>
-          {/* <CatalogList /> */}
-        </section>
-      </>
+            <CatalogList products={favorites} />
+          </section>
+        </>
+      ) : (
+        <FavouritesEmpty />
+      )}
     </Layout>
   );
 }
