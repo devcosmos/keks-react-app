d16aa484a2dcd7fc5e655faf96872deb399b70e5
cosmos <va-litv@yandex.ru>
2023-07-07 15:44:24 +0300

    get product from server

--- src/store/products-data/selectors.ts
+++ src/store/products-data/selectors.ts
@@ -1,8 +1,9 @@
 import { NameSpace } from '../../consts';
-import { Categories, Products } from '../../types/products';
+import { Categories, ProductFullInfo, Products } from '../../types/products';
 import { State } from '../../types/state';
 
 export const getProducts = (state: State): Products => state[NameSpace.Data].products;
+export const getProduct = (state: State): ProductFullInfo | null => state[NameSpace.Data].product;
 export const getCategories = (state: State): Categories => state[NameSpace.Data].categories;
 export const getLoadingStatus = (state: State): boolean => state[NameSpace.Data].isLoading;
 export const getErrorStatus = (state: State): boolean => state[NameSpace.Data].isError;
