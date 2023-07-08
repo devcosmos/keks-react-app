8effdfe1adc6c95f6bd8455653dc12e96405ccd8
cosmos <va-litv@yandex.ru>
2023-07-08 15:47:11 +0300

    add status selectors for review

--- src/store/reviews-data/selectors.ts
+++ src/store/reviews-data/selectors.ts
@@ -4,3 +4,5 @@ import { NameSpace } from '../../consts';
 import { State } from '../../types/state';
 
 export const getReviews = (state: State): Reviews => state[NameSpace.ReviewsData].reviews;
+export const getReviewsLoadingStatus = (state: State): boolean => state[NameSpace.ReviewsData].isLoading;
+export const getReviewsErrorStatus = (state: State): boolean => state[NameSpace.ReviewsData].isError;
