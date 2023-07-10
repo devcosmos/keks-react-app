6bc0e85695b0c77c9611c61edf0b324c3b677dff
cosmos <va-litv@yandex.ru>
2023-07-11 00:15:49 +0300

    add location type

---
+++ src/types/locations.ts
@@ -0,0 +1,17 @@
+export type Coordinates = {
+  latitude: number;
+  longitude: number;
+};
+
+export type Location = {
+  id: string;
+  name: string;
+  address: string;
+  latitude: number;
+  longitude: number;
+  icon: string;
+}
+
+export type Locations = Location[];
+
+
