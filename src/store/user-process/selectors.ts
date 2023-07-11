9dd56c455bc2a191660ab1c5a12f354488218d32
cosmos <va-litv@yandex.ru>
2023-07-11 13:52:07 +0300

    add request status to user process

--- src/store/user-process/selectors.ts
+++ src/store/user-process/selectors.ts
@@ -1,6 +1,8 @@
-import { AuthStatus, NameSpace } from '../../consts';
+import { AuthStatus, NameSpace, RequestStatus } from '../../consts';
 import { UserData } from '../../types/users';
 import { State } from '../../types/state';
 
 export const getAuthStatus = (state: State): AuthStatus => state[NameSpace.User].authorizationStatus;
 export const getCurrentUser = (state: State): UserData | null => state[NameSpace.User].user;
+export const getSingUpRequestStatus = (state: State): RequestStatus => state[NameSpace.User].singUpRequestStatus;
+export const getSingInRequestStatus = (state: State): RequestStatus => state[NameSpace.User].singInRequestStatus;
